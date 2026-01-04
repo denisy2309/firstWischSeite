3845b9d77714862f719c627ac2b3992c53018203
Denis <deniscan62@hotmail.com>
2026-01-05 00:27:41 +0100

    Configure CORS to allow requests from the specified origin

--- Backend/server.js
+++ Backend/server.js
@@ -4,7 +4,9 @@ const app = express();
 const PORT = 3000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: 'https://denisy2309.github.io'
+}));
 app.use(express.json());
 app.use(express.static('public')); // Statische Dateien (HTML, CSS, JS)
 
